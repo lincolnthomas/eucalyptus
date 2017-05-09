306c350d27578a0b84139b05cded14d19f843aee
Lincoln Thomas <lincoln.thomas@hpe.com>
2017-05-08 18:30:13 -0700

    SUP-706 SC crash with Ceph Added ssh-ing to CLC from SC

--- tools/clean-ceph-deleted.sh
+++ tools/clean-ceph-deleted.sh
@@ -27,6 +27,6 @@ EUCAJARS=$(ls /usr/share/eucalyptus/*.jar|tr '\n' ':')
 JAVAPARAMS="-Xbootclasspath/p://///usr/share/eucalyptus/openjdk-crypto.jar -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+DisableExplicitGC -javaagent:///usr/share/eucalyptus/aspectjweaver-1.8.8.jar -Djava.net.preferIPv4Stack=true -Djava.library.path=///usr/lib/eucalyptus -Djava.awt.headless=true -Deuca.home=// -Deuca.db.home= -Deuca.extra_version= -Deuca.var.dir=///var/lib/eucalyptus -Deuca.state.dir=///var/lib/eucalyptus -Deuca.run.dir=///var/run/eucalyptus -Deuca.lib.dir=///usr/share/eucalyptus -Deuca.libexec.dir=///usr/lib/eucalyptus -Deuca.conf.dir=///etc/eucalyptus/cloud.d -Deuca.log.dir=///var/log/eucalyptus -Deuca.jni.dir=///usr/lib/eucalyptus -Djava.util.prefs.PreferencesFactory=com.eucalyptus.util.NoopPreferencesFactory -Deuca.log.exhaustive.db=FATAL -Deuca.log.exhaustive.cc=FATAL -Deuca.log.exhaustive.user=FATAL -Deuca.log.exhaustive.external=FATAL -Deuca.log.exhaustive=FATAL -Deuca.version=4.4.0 -Deuca.log.level=TRACE -Deuca.log.appender=console-log -Deuca.extra_version=0.25812.3.el7 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=///var/log/eucalyptus/ -Deuca.user=eucalyptus -Xms8000m -Xmx63000m -XX:ErrorFile=/var/log/eucalyptus/hs_err_pid%p.log -Dfile.encoding=UTF-8 -cp"
 
 echo $0 starting CephCleaner
-java $JAVAPARAMS $EUCAJARS com.eucalyptus.blockstorage.CephCleaner
+java $JAVAPARAMS $EUCAJARS com.eucalyptus.blockstorage.CephCleaner $1
 echo $0 finished CephCleaner
 
